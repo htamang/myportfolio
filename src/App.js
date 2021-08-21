ee3bf73a03ec318110d9f9664800bd68b6f53ee2
htamang <83646121+htamang@users.noreply.github.com>
2021-08-21 10:40:38 +1000

    Project Section added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
-// import Projects from './components/Projects';
+import Projects from './components/Projects';
 // import Contact from './components/Contact';
 // import Footer from './components/Footer';
 
@@ -19,8 +19,8 @@ function App() {
       <Home />
       <About />
       <Skills />
-      {/* <Projects />
-      <Contact />
+      <Projects />
+      {/* <Contact />
       <Footer />  */}
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,87 @@
-import React from 'react'
+import React from "react";
 
 const Projects = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <section id="projects">
+      <div class="container-fluid px-0 projects-section">
+        <div class="container">
+          <div class="row">
+            <div class="card col-sm-4 mb-5">
+              <h1>Quizz</h1>
+              <p>
+                An IT professional looking for an entry-level role of developer
+                programmer/software developer in a company with a dynamic team
+                that can help me in enhancing my technical skills and
+                strengthening my knowledge.
+              </p>
 
-export default Projects
+              {/* <a class="learn-more" href="#">Demo</a>
+                  <a class="learn-more" href="#">Code</a> */}
+
+              <div class="mt-4">
+                <button class="white-btn">
+                  Demo<i class="fas fa-angle-right ps-2"></i>
+                </button>
+              </div>
+            </div>
+            <div class="card col-sm-4 mb-5">
+              <h1>Weather App</h1>
+              <p>
+                An IT professional looking for an entry-level role of developer
+                programmer/software developer in a company with a dynamic team
+                that can help me in enhancing my technical skills and
+                strengthening my knowledge.
+              </p>
+
+              {/* <!--
+                    <a class="learn-more" href="#">Demo</a>
+                <a class="learn-more" href="#">Code</a>
+                  --> */}
+              <div class="mt-4">
+                <button class="white-btn">
+                  Demo<i class="fas fa-angle-right ps-2"></i>
+                </button>
+              </div>
+            </div>
+            <div class="card col-sm-4 mb-5">
+              <h1>Restaurant</h1>
+              <p>
+                An IT professional looking for an entry-level role of developer
+                programmer/software developer in a company with a dynamic team
+                that can help me in enhancing my technical skills and
+                strengthening my knowledge.
+              </p>
+              {/* <!--
+                    <a class="learn-more" href="#">Demo</a>
+                <a class="learn-more" href="#">Code</a>
+                  --> */}
+              <div class="mt-4">
+                <button class="white-btn">
+                  Demo<i class="fas fa-angle-right ps-2"></i>
+                </button>
+              </div>
+            </div>
+            <div class="card col-sm-4 mb-5">
+              <h1>Face Recognition</h1>
+              <p>
+                An IT professional looking for an entry-level role of developer
+                programmer/software developer in a company with a dynamic team
+                that can help me in enhancing my technical skills and
+                strengthening my knowledge.
+              </p>
+              {/* <!-- <a class="learn-more" href="#">Demo</a> 
+                <a class="learn-more" href="#">Code</a> --> */}
+              <div class="mt-4">
+                <button class="white-btn">
+                  Demo<i class="fas fa-angle-right ps-2"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
